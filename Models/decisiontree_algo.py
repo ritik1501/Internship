a087749787a30c74ca509d13ae4b70ffcd368396
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-05-15 19:32:28 +0530

    code and pickle upload in models

---
+++ Models/decisiontree_algo.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import preprocessing
+import pickle
+
+
+file = "data.csv"
+data = pd.read_csv(file, sep=',', header=0)
+con = preprocessing.LabelEncoder()
+predict = 'infected'
+
+X = np.array(data.drop([predict], 1))
+Y = np.array(con.fit_transform(list(data[predict])))
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=100)
+
+
+try:
+    pickle_in = open("decision.pickle", "rb")
+    clf_entropy = pickle.load(pickle_in)
+
+
+except:
+    clf_entropy = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=7, min_samples_leaf=5)
+    with open("decision.pickle", "wb") as f:
+        pickle.dump(clf_entropy, f)
+
+
+clf_entropy.fit(X_train, Y_train)
+
+acc = clf_entropy.score(X_test, Y_test)
+print(acc)
+
+prediction = clf_entropy.predict(X_test)
+
+'''for i in range(len(prediction)):
+    print(prediction[i], '  ', Y_test[i])'''
+
+
+manual_data = np.array([[7, 9, 10, 12]])
+manual_predict = clf_entropy.predict(manual_data)
+print("\n\nRESULT :  ", manual_predict)
+
+
+
+
+
+
+
+
+
+
