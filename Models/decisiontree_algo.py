1d6c3f1c9aede6ac9886b4490bdae343ed858ca4
Krishanu Bhattachajee <47850005+Kaiserb11@users.noreply.github.com>
2020-05-18 16:35:26 +0530

    added update2

--- Models/decisiontree_algo.py
+++ Models/decisiontree_algo.py
@@ -26,9 +26,7 @@ except:
     clf_entropy = DecisionTreeClassifier(criterion="entropy", random_state=100, max_depth=7, min_samples_leaf=5)
     with open("decision.pickle", "wb") as f:
         pickle.dump(clf_entropy, f)
-
-
-clf_entropy.fit(X_train, Y_train)
+        clf_entropy.fit(X_train, Y_train)
 
 acc = clf_entropy.score(X_test, Y_test)
 print(acc)
