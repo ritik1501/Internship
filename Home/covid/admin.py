059941ec7975548a2a6d2329b3173fcd0db988b4
ritik <ritik-01>
2020-05-21 19:32:17 +0530

    Form Commit

--- Home/covid/admin.py
+++ Home/covid/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from covid.models import UserData
+from covid.models import UserData, LargeData
 
 # Register your models here.
 admin.site.register(UserData)
+admin.site.register(LargeData)
+
+admin.site.site_header = "COVID-19 Admin"
+admin.site.site_title = "COVID-19 Admin Portal"
+admin.site.index_title = "Welcome to COVID-19 Checker Portal"
--- Home/covid/models.py
+++ Home/covid/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime   
 
 # Create your models here.
 class UserData(models.Model):
@@ -8,8 +9,30 @@ class UserData(models.Model):
     cough = models.IntegerField()
     throat = models.IntegerField()
     breathing = models.IntegerField()
-    result = models.FloatField()
-    timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+    result = models.IntegerField()
+    timestamp = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return 'User name '+ self.name
+
+class LargeData(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40)
+    age = models.IntegerField()
+    gender = models.IntegerField()
+    fever = models.IntegerField()
+    cough = models.IntegerField()
+    throat = models.IntegerField()
+    weakness = models.IntegerField()
+    diffBreath = models.IntegerField()
+    drowsiness = models.IntegerField()
+    pain = models.IntegerField()
+    travel = models.IntegerField()
+    symptoms = models.IntegerField()
+    blood = models.IntegerField()
+    appetide = models.IntegerField()
+    result = models.IntegerField()
+    timestamp = models.DateTimeField(blank=True, default=datetime.now)
 
     def __str__(self):
         return 'User name '+ self.name
